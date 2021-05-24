d81a2f4d4ebb98392c84e2728042fe39e9517f0c
jamessonlps <jamesson1233@gmail.com>
2021-05-24 11:05:11 -0300

    (front+back) carrega visualizacao de pdf do back

--- frontend/components/Main.js
+++ frontend/components/Main.js
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
-import { ActivityIndicator, View, Button, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { View, Button, StyleSheet, Text, SafeAreaView } from 'react-native';
 import { Asset } from 'expo-asset';
 import PDFReader from 'rn-pdf-reader-js';
 
-export default function Main(props) {
-    const file = require('../files/cad.pdf');
+import client from '../client';
 
-    const [uri, setURI] = useState(null);
+function stringify(body) {
+    return JSON.stringify(body, null, 2);
+}
+
+export default function Main(props) {
+    const DEFAULT_BODY = {
+        "codigo": 0,
+        "titulo": "0",
+        "planta": "0"
+    };
+
+    const [url, setURL] = useState('http://10.0.2.2:8080/pavimento?codigo=1313');
+    const [body, setBody] = useState(stringify(DEFAULT_BODY));
+    const [response, setResponse] = useState('');
+
     const [loading, setLoading] = useState(true);
 
     const [viewDocument, setViewDocument] = useState(false);
+    const [pdf, setPdf] = useState('');
 
-    Asset.loadAsync(file)
-        .then(() => {
-            if (loading) {
-                const asset = Asset.fromModule(file);
-                setURI(asset.localUri);
-                setLoading(false);
-            }
-        });
-    
+
     return(
         <>
-            {viewDocument ? <PDFReader style={{paddingTop: 30}} source={{uri: uri}}/>
+            {viewDocument ? <PDFReader style={{paddingTop: 30}} source={{uri: pdf}}/>
             :
             <SafeAreaView style={styles.documentPost}>
                 <Text>Planta baixa D. R. Bias</Text>
@@ -30,11 +36,25 @@ export default function Main(props) {
                     <Button 
                         title="Visualizar" 
                         onPress={() => {
-                            setViewDocument(true);
+                            client.get(
+                                "http://192.168.1.111:8080/pavimento?codigo=1313",
+                                (body) => {
+                                    setPdf(body.planta);
+                                    setResponse(stringify(body));
+                                    setViewDocument(true);
+                                },
+                                (message) => setResponse(message),
+                                () => setLoading(false),
+                                () => setLoading(false)
+                            );
                         }}
                     />
                     <Button title=" Detalhes " />
                 </View>
+
+                <Text>{response}</Text>
+                <Text>{pdf}</Text>
+
             </SafeAreaView>
             }
         </>
